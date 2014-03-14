3d175d0df5fbe0724aa4d9f313ad7f1a08038418
srwilliams <srwilliams444@gmail.com>
2014-03-14 00:29:07 -0500

    movement for invaders updated

--- main1.cc
+++ main1.cc
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctime>
+#include <vector>
 
 #include "allegro.h"
 #include "classes.h"
@@ -16,6 +17,7 @@ int main(void) {
     allegro_init();
     install_keyboard();
     srand(time(NULL));
+    
 
     int ret = set_gfx_mode(MODE, WIDTH, HEIGHT, 0, 0);
     if (ret !=0) {
@@ -25,10 +27,18 @@ int main(void) {
 
     set_color_depth(8);
     
-    //Invader invader (20);
+    Invader invader (20);
+    ///////// Load some invaders /////////
+    //std::vector<Invader> row1;
+
+
     Player player (3);
     Guard guard (70);
-
+    
+    // Double buffering //
+    BITMAP *buffer = NULL;
+    buffer = create_bitmap(640, 480);
+    clear_bitmap(buffer);
 
     ///////// Initialize Invaders //////////////////
     Invader *invaderArmy[5];
@@ -38,8 +48,9 @@ int main(void) {
     Invader *invaderRow4[12];    // and one row of 40-point invaders
     Invader *invaderRow5[12];
 
-    for (int i=0; i<12, i++) {
-
+    //for (int i=0; i<12; i++) {
+    //    row1.push_back(invader);
+    //}
 
 
 
@@ -64,10 +75,10 @@ int main(void) {
     while (!key[KEY_ESC]) {
 
         clear_keybuf();
-        hline(screen, 0, 0, 638, makecol(0, 204, 0));
-        hline(screen, 0, 478, 638, makecol(0, 204, 0));
-        vline(screen, 0, 0, 478, makecol(0, 204, 0));
-        vline(screen, 638, 0, 478, makecol(0, 204, 0));
+        hline(buffer, 0, 0, 638, makecol(0, 204, 0));
+        hline(buffer, 0, 478, 638, makecol(0, 204, 0));
+        vline(buffer, 0, 0, 478, makecol(0, 204, 0));
+        vline(buffer, 638, 0, 478, makecol(0, 204, 0));
 
         rectfill(screen, x, y, x+(player.plyr->w), y+(player.plyr->h), 0);
         rectfill(screen, a, b, a+invader.invdr[0]->w, b+invader.invdr[0]->h, 0);
@@ -86,16 +97,28 @@ int main(void) {
 
         // Animations for the invaders //
 
-        if (a <WIDTH - (2+invader.invdr[0]->w)) {
-            a+=2;
-            invader.x = a;
-        }
+        //for(int i=0; i<122; i++) {
+        //    (row1[i]).x += i+5;
+        //}
+        
+        //if (a <WIDTH - (2+invader.invdr[0]->w)) {
+        //    a=2;
+        //    invader.x += a;
+        //}
 
         if (framecount++ > framedelay) {
             framecount = 0;
             curframe++;
+            
             if (curframe >1) {
+                invader.x +=15;
+                if (invader.x ==620) invader.x = 0;
                 curframe = 0;
+            }
+            else if (curframe ==1) {
+                invader.x +=15;
+                if (invader.x ==638) invader.x = 0;
+       
             }
         }
         ///////////////////////////////////
@@ -106,11 +129,11 @@ int main(void) {
 
         Bullet bullet;
         if (key[KEY_SPACE]) { 
-            bullet.i = x;
-            bullet.j = y;
+            bullet.i = x+10;
+            bullet.j = y-12;
         }
 
-        bullet.j -=2;
+        bullet.j -=4;
 
         if (bullet.j == 0) {
             destroy_bitmap(bullet.bllt);
@@ -119,16 +142,32 @@ int main(void) {
 
 
 
+
+        ///// Draw everything to the screen /////
+
+        clear_bitmap(buffer);
+        draw_sprite(buffer, player.plyr, x, y);
+        //for (int i =0; i<12; i++) {
+            draw_sprite(buffer, invader.invdr[curframe], invader.x, invader.y);
+        //}
+        draw_sprite(buffer, guard.grd, guard.x, guard.y);
+        draw_sprite(buffer, bullet.bllt, bullet.i, bullet.j);
         draw_sprite(screen, player.plyr, x, y);
-        draw_sprite(screen, invader.invdr[curframe], invader.x, invader.y);
+        //for (int i = 0; i<12; i++) {
+            draw_sprite(screen, invader.invdr[curframe], invader.x, invader.y);
+        //}
         draw_sprite(screen, guard.grd, guard.x, guard.y);
         draw_sprite(screen, bullet.bllt, bullet.i, bullet.j);
+        vsync();
+        blit(buffer, screen, 0,0,0,0,640,480);
         rest(1);
+        ////////////////////////////////////////
     }
 
     destroy_bitmap(player.plyr);
     destroy_bitmap(invader.invdr[0]);
     destroy_bitmap(invader.invdr[1]);
+    destroy_bitmap(buffer);
     allegro_exit();
     return 0;
 }
